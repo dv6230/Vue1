cd12de2a8594e7d71245c1ff3b5cb9f2f8fd9e60
allisonhung <allisonhung@iscom.com.tw>
2022-09-14 15:45:18 +0800

    加密套件

--- src/js/FetchAPI.js
+++ src/js/FetchAPI.js
@@ -1,3 +1,5 @@
+import CryptoJS from '../../node_modules/crypto-js'
+
 export function fetchPostApi(url, parameter) {
     url = process.env.VUE_APP_API_HOST + url
 
@@ -11,4 +13,26 @@ export function fetchPostApi(url, parameter) {
 
 }
 
+export function loginAPI(parameter) {
+
+    let url = process.env.VUE_APP_API_HOST + 'api/test/Decode'
+    let secretKey = CryptoJS.enc.Utf8.parse(process.env.VUE_APP_ENCODE_KEY)
+    let iv = CryptoJS.enc.Utf8.parse(process.env.VUE_APP_ENCODE_IV)
+
+    const ciphertext = CryptoJS.AES.encrypt(parameter, secretKey,
+        {
+            keySize: 128 / 8,
+            iv: iv,
+            mode: CryptoJS.mode.CBC,
+            padding: CryptoJS.pad.Pkcs7
+        }).toString();
+
+    return fetch(url, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(ciphertext)
+    }).then(response => response.text())
+}
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,12 +11,16 @@
         <input class="form-control" type="text" v-model="editContent">
         <button class="btn btn-success mt-3" @click="click()">送出</button>
       </div>
+      <div class="col-6">
+        {{ encode }} <br> {{ decode }}
+      </div>
     </div>
   </div>
 </template>
 
 <script>
 import * as fetchapi from '../js/FetchAPI'
+import CryptoJS from 'crypto-js'
 
 export default {
   name: 'HomeView',
@@ -24,7 +28,9 @@ export default {
     return {
       message: '',
       vuexContent: '',
-      editContent: ''
+      editContent: '',
+      encode: '',
+      decode: ''
     }
   },
   created() {
@@ -37,10 +43,20 @@ export default {
   methods: {
     click() {
       this.$store.dispatch('updateTest', this.editContent)
-      this.updateContent()
+      this.updateContent()      
     },
     updateContent() {
       this.vuexContent = this.$store.state.test
+
+      // const CryptoJS = require('crypto-js')
+      // const secretKey = 'ThisIsMyKey'
+      // const ciphertext = CryptoJS.AES.encrypt(this.vuexContent, secretKey).toString()
+      // this.encode = ciphertext
+      // const originalText = CryptoJS.AES.decrypt(ciphertext, secretKey).toString(CryptoJS.enc.Utf8)
+      // this.decode = originalText
+
+      fetchapi.loginAPI(this.vuexContent).then(result => this.decode = result)
+
     }
   }
 };
