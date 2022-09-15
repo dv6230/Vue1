0ff3d677e362335ba0b51169e599b55b775fec77
allisonhung <allisonhung@iscom.com.tw>
2022-09-15 16:26:22 +0800

    layout 調整

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -11,6 +11,8 @@
         <input class="form-control" type="text" v-model="editContent">
         <button class="btn btn-success mt-3" @click="click()">送出</button>
       </div>
+    </div>
+    <div class="d-flex justify-content-center mt-3">
       <div class="col-6">
         {{ encode }} <br> {{ decode }}
       </div>
@@ -43,7 +45,7 @@ export default {
   methods: {
     click() {
       this.$store.dispatch('updateTest', this.editContent)
-      this.updateContent()      
+      this.updateContent()
     },
     updateContent() {
       this.vuexContent = this.$store.state.test
