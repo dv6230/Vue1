b24fabcfe2ab966ba7ce260580b191e5826acedb
allisonhung <allisonhung@iscom.com.tw>
2022-09-16 09:41:18 +0800

    加入 vue router

---
+++ src/views/Information.vue
@@ -0,0 +1,25 @@
+<template lang="">
+    <div id="info">
+        <h3 class="mb-3">Vuex 測試</h3>
+
+    </div>
+</template>
+<script>
+export default {
+    name: 'InfoView',
+    created() {
+
+    },
+    data() {
+        return {
+
+        }
+    },
+    methods: {
+
+    },
+}
+</script>
+<style lang="">
+    
+</style>
